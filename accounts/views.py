99c08f03d7d619e7fa5b90df897d77a82df23a9c
Alekiie <amnjogu78@gmail.com>
2025-11-21 22:04:42 +0300

    FEAT: Update permission_classes in UserViewSet to include IsAdminOrManager for enhanced access control

--- accounts/views.py
+++ accounts/views.py
@@ -2,14 +2,14 @@ from rest_framework import serializers, viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import CustomUser
 from .serializers import UserSerializer,CustomTokenObtainPairSerializer
-from .permissions import IsAdmin
+from .permissions import IsAdmin,IsAdminOrManager
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated, IsAdmin]
+    permission_classes = [IsAuthenticated, IsAdminOrManager]
 
 class CustomLoginView(TokenObtainPairView):
     serializer_class=CustomTokenObtainPairSerializer
